7a559aeb83f4a0709aab4cee56c6e42bc768d12a
nicolashacku <nicolasjimenez089@gmail.com>
2025-12-09 02:11:13 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pareja-dashboard/sw.js', { scope: '/pareja-dashboard/' })})}
